b659c57efe900f50511897714965e1307940f915
Juan Carlos Ferrel <juancafe2@gmail.com>
2017-09-28 20:06:28 -0400

    Chat adapter set uo bubble chat

---
+++ app/src/androidTest/java/ChatAdapter.java
@@ -0,0 +1,35 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by juancarlos on 9/28/17.
+ */
+
+public class ChatAdapter extends BaseAdapter {
+    private static LayoutInflater inflater = null;
+
+    public ChatAdapter(Activity activity, ArrayList&lt;ChatMessage&gt; list)
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
